5ab254cbb372c05a71ccec56c53673b4805b504e
Malik <malikoabdulaziz@gmail.com>
2021-09-27 12:07:39 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ data: 'Welcome to the home endpoint' });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`app is now listening on port ${port}`);
+});
