2950ed5105da0cf5e4e4c35ac36a7465f58fff2c
Malik <malikoabdulaziz@gmail.com>
2021-09-30 09:41:20 +0100

    one time init db
    heroku postgress deploy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const routes = require('./routes');
+const db = require('./db');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
